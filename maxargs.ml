6b4179a3c4893a7c4c40befee880664a89a7ab4e
Mark Johnson <luther07@gmail.com>
2011-08-22 14:16:32 -0400

    add maxargs fun for ocaml

---
+++ maxargs.ml
@@ -0,0 +1,9 @@
+(* Write an ML function (maxargs : stm->int) that tells the maximum number
+ * of arguments of any print statement within any subexpression of a
+ * given statement. For example, maxargs(prog) is 2. *)
+fun maxargs(CompoundStm(a,b))
+    = 1
+   |maxargs(AssignStm(a,b))
+    = 0
+   |PrintStm(a)
+    = 0

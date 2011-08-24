c71006cbe7d2475c92b6ce8f20b9e394957772fe
Mark Johnson <luther07@gmail.com>
2011-08-24 08:09:01 -0400

    working on maxargs function...

--- OCaml/maxargs.ml
+++ OCaml/maxargs.ml
@@ -1,11 +1,11 @@
 (* Write an OCaml function (maxargs : stm->int) that tells the maximum number
  * of arguments of any print statement within any subexpression of a
  * given statement. For example, maxargs(prog) is 2. *)
-let rec maxargs_in_stm =
+let rec (maxargs_in_stm : Stm -> int) =
    function
       CompoundStm(a,b)
        -> 1 (* I don't know yet *)
      |AssignStm(a,b)
        -> 0
-     |PrintStm(a)
-       -> 1 (* I don't know yet *)
+     |PrintStm(x::xs)
+       -> 

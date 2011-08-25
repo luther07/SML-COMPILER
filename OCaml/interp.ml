42a0487599d1395efa99e39dd497e0a9f2072468
Mark Johnson <luther07@gmail.com>
2011-08-25 14:46:09 -0400

    programming exercise 2, comment is the problem statement

---
+++ OCaml/interp.ml
@@ -0,0 +1,4 @@
+(* Write an ML function interp: stm -> unit that interprets a program in this
+ * language. To write in a "functional" style, -- without assignment (:=) or
+ * arrays -- maintain a list of (variable, integer) pairs, and produce new
+ * versions of this list at each AssignStm. *)

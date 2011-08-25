ffabfc1f39c8824af1795cababc2652f5c757f23
Mark Johnson <luther07@gmail.com>
2011-08-25 14:16:35 -0400

    developing pattern matching on the PrintStm, works good

--- OCaml/maxargs.ml
+++ OCaml/maxargs.ml
@@ -1,11 +1,13 @@
 (* Write an OCaml function (maxargs : stm->int) that tells the maximum number
  * of arguments of any print statement within any subexpression of a
  * given statement. For example, maxargs(prog) is 2. *)
-let rec (maxargs_in_stm : Stm -> int) =
+let rec (maxargs : stm -> int) =
    function
       CompoundStm(a,b)
        -> 1 (* I don't know yet *)
      |AssignStm(a,b)
        -> 0
+     |PrintStm([])
+       -> 0
      |PrintStm(x::xs)
-       -> 
+       -> 1 + maxargs(PrintStm(xs))

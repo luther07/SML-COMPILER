723a0ef2776dbdef16a3fe05f579e3886aab63dd
Mark Johnson <luther07@gmail.com>
2011-11-03 20:55:12 -0500

    update

--- OCaml/maxargs.ml
+++ OCaml/maxargs.ml
@@ -1,7 +1,7 @@
 (* Write an OCaml function (maxargs : stm->int) that tells the maximum number
  * of arguments of any print statement within any subexpression of a
  * given statement. For example, maxargs(prog) is 2.
- * For part 1, remeber that print statements can contain expressions that 
+ * For part 1, remember that print statements can contain expressions that 
  * contain other print statement.
  *
  * TODO: pattern-match for case where a print statement contains other
@@ -14,6 +14,9 @@ let rec (maxargs : stm -> int) =
        -> 0
      |PrintStm([])
        -> 0
+     |PrintStm([EseqExp(PrintStm(x::xs),expr)::eseq_exp_list])
+       -> if 1 + maxargs(PrintStm(eseq_exp_list)) >= 
+             
      |PrintStm(x::xs)
        -> 1 + maxargs(PrintStm(xs))
 and (maxargs_exp : exp -> int) =
